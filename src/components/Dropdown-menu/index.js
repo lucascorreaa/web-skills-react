0633af287d624ad7c201ca8219bee5879271aa18
Lucas do Espirito Santo Correa <drt60337@MAC454846-M.local>
2024-04-01 15:21:35 -0300

    feat: i adjusted the app to test the components and create a base structure

---
+++ src/components/Dropdown-menu/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import './styles.css'
+import menu from './assets/images/mi_check_list_icon.svg'
+import edit from './assets/images/edit.png'
+import inbox from './assets/images/inbox.png'
+import logout from './assets/images/logout.png'
+import help from './assets/images/help.png'
+import settings from './assets/images/settings.png'
+import user from './assets/images/user.png'
+
+function DropdownMenu() {
+  const [open, setOpen] = useState(false)
+  return (
+    <div>
+      <div className="menu-container">
+        <div className="menu-trigger" onClick={() => {setOpen(!open)}}>
+          <img src={menu} alt="" />
+        </div>
+
+        {
+          open &&
+          <div className="dropdown-menu">
+              <h3>The Lucas <br/> <span>WebSite Designer</span></h3>
+              <ul>
+                <DropdownItem img={user} text="My Profile" /> 
+                <DropdownItem img={edit} text="edit" /> 
+                <DropdownItem img={inbox} text="inbox" /> 
+                <DropdownItem img={settings} text="settings" /> 
+                <DropdownItem img={help} text="help" /> 
+                <DropdownItem img={logout} text="logout" /> 
+             </ul>
+          </div>
+        
+        }
+      </div>
+    </div>
+  );
+}
+
+function DropdownItem(props) {
+  return (
+
+      <li className="dropdownItem">
+        <img src={props.img} alt="" />
+        <a href=""> {props.text} </a>
+      </li>
+
+  )
+}
+
+export default DropdownMenu;
